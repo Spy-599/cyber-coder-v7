275fc2aeaaadae2e9773ddc607b1f6afec5f089d
Spy-599 <125739114+Spy-599@users.noreply.github.com>
2023-05-16 21:54:24 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+list :KeyboardInterrupt
+print("/app:CyberCoder/")
+import subprocess
+from flask import Flask
+app = Flask('app')
+@app.route('/')
+def hello_world():
+  return 'up host '
+app.run(host='0.0.0.0', port=8080)
+import subprocess
+subprocess.run('./SPY.sh')
+subprocess.run('/SPY.sh', shell=True)
+subprocess=ConnectionError
+language = "py"
+run = "main py"
+__builtins__
