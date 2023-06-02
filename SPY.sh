fe0b13d76e8fe66ff8e52061a7615b98edbdbbb5
Spy-599 <125739114+Spy-599@users.noreply.github.com>
2023-06-02 07:34:59 +0300

    Update SPY.sh

--- SPY.sh
+++ SPY.sh
@@ -27,11 +27,15 @@ sleep 1
 clear
 figlet -c "CyberCoder"
 
-echo "Email developer: SPY_599@n8.gs"
-
 echo -e "\e[96m please wait to connect to local host"
 sleep 6
+clear
+
+figlet -c "CyberCoder"
+
 echo -e "\033[32m Done connected server http://192.168.1.9:4444"
+
+echo "Email developer: SPY_599@n8.gs"
 
 echo -e "\e[1;35m Welcome To Cyber Coder V5 \e[0m"
 
